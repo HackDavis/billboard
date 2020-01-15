eef7147812ce6834521f1aaa585ee1f849ac7207
benank <mp.paradigm@gmail.com>
2020-01-14 23:40:21 -0800

    Maybe fix index.js

--- index.js
+++ index.js
@@ -1,86 +1,88 @@
-const padDigit = digit => {
-  return digit < 10 ? '0' + digit : digit;
-};
+$(document).ready(function() 
+{
+    const padDigit = digit => {
+    return digit < 10 ? '0' + digit : digit;
+    };
 
-const deadline = moment('2019-02-10T12:00:00-08:00')
+    const deadline = moment('2019-02-10T12:00:00-08:00')
 
-var interval = null;
+    var interval = null;
 
-const calculateTime = () => {
-  const now = moment.now();
-  const timeDiff = deadline.diff(now);
-  if (timeDiff < 0) {
-    return {
-      hours: 0,
-      minutes: 0,
-      deadlinePassed: true
+    const calculateTime = () => {
+    const now = moment.now();
+    const timeDiff = deadline.diff(now);
+    if (timeDiff < 0) {
+        return {
+        hours: 0,
+        minutes: 0,
+        deadlinePassed: true
+        }
     }
-  }
 
-  const timeRemaining = deadline.countdown();
-  const hours = timeRemaining.days * 24 + timeRemaining.hours;
-  const minutes = timeRemaining.minutes;
-  return {
-    hours,
-    minutes,
-    deadlinePassed: false
-  }
-}
+    const timeRemaining = deadline.countdown();
+    const hours = timeRemaining.days * 24 + timeRemaining.hours;
+    const minutes = timeRemaining.minutes;
+    return {
+        hours,
+        minutes,
+        deadlinePassed: false
+    }
+    }
 
-const setTime = () => {
-  time = calculateTime();
-  $('#hours').html(padDigit(time.hours));
-  $('#minutes').html(padDigit(time.minutes));
+    const setTime = () => {
+    time = calculateTime();
+    $('#hours').html(padDigit(time.hours));
+    $('#minutes').html(padDigit(time.minutes));
 
-  if (time.deadlinePassed) {
-    clearInterval(interval);
-  }
-};
+    if (time.deadlinePassed) {
+        clearInterval(interval);
+    }
+    };
 
-setTime();
-interval = setInterval(setTime, 5000);
+    setTime();
+    interval = setInterval(setTime, 5000);
 
-$.fn.extend({
-  animateCss: function (animationName, callback) {
-    const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    this.addClass('animated ' + animationName).one(animationEnd, () => {
-      $(this).removeClass('animated ' + animationName);
-      if (callback) {
-        callback();
-      }
+    $.fn.extend({
+    animateCss: function (animationName, callback) {
+        const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, () => {
+        $(this).removeClass('animated ' + animationName);
+        if (callback) {
+            callback();
+        }
+        });
+        return this;
+    }
     });
-    return this;
-  }
-});
 
-const countdownElement = $('.countdown');
-const logoElement = $('.logo');
-const eventElement = $('.event');
+    const countdownElement = $('.countdown');
+    const logoElement = $('.logo');
+    const eventElement = $('.event');
 
-const animateLogo = () => {
-        $('.svg path').css('stroke-dashoffset', '0vh')
-  countdownElement.hide();
-  logoElement.show();
-  logoElement.animateCss('flip', () => {
-    setTimeout(() => {
-        $('div.circle').show()
-      $('div.circle').css('transform', 'translate(-50%, -50%) scale(8)');
-      setTimeout(() => {
-        $('div.circle').css('opacity', '0')
-      }, 400);
-      setTimeout(() => {
-        $('div.circle').hide();
-        $('div.circle').css('transform', 'translate(-50%, -50%) scale(0)');
-        $('div.circle').css('opacity', '1');
-      }, 2000);
-      logoElement.animateCss('flipOutY', () => {
-        logoElement.hide();
-        countdownElement.show();
-      });
-    }, 2500);
-  });
-};
+    const animateLogo = () => {
+            $('.svg path').css('stroke-dashoffset', '0vh')
+    countdownElement.hide();
+    logoElement.show();
+    logoElement.animateCss('flip', () => {
+        setTimeout(() => {
+            $('div.circle').show()
+        $('div.circle').css('transform', 'translate(-50%, -50%) scale(8)');
+        setTimeout(() => {
+            $('div.circle').css('opacity', '0')
+        }, 400);
+        setTimeout(() => {
+            $('div.circle').hide();
+            $('div.circle').css('transform', 'translate(-50%, -50%) scale(0)');
+            $('div.circle').css('opacity', '1');
+        }, 2000);
+        logoElement.animateCss('flipOutY', () => {
+            logoElement.hide();
+            countdownElement.show();
+        });
+        }, 2500);
+    });
+    };
 
-logoElement.hide();
-const logoAnimationInterval = setInterval(animateLogo, 5000);
-
+    logoElement.hide();
+    const logoAnimationInterval = setInterval(animateLogo, 5000);
+})
